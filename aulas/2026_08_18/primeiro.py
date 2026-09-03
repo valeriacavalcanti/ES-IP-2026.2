82c3f62e4d93cccab1e8aa58e8aca3930a2450dc
Valeria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2026-09-03 09:30:59 -0300

    Aula 18/08/2026

---
+++ aulas/2026_08_18/primeiro.py
@@ -0,0 +1,14 @@
+#nome = 'Valéria Maria'
+nome = input('Digite seu nome: ')
+
+#idade = '15'
+idade = input('Digite sua idade: ')
+idade = int(idade)
+
+# calcular o dobro da idade
+dobro_da_idade = idade * 2
+
+print(nome, idade)
+# print(idade)
+print(dobro_da_idade)
+print('eita')
